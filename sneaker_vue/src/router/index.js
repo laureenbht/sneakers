b9f4007b5a0b102730c725d43543331f15377f13
laureenbht <laureen.bahuaut@epitech.digital>
2023-12-13 22:04:38 +0100

    add old repo without node_module and wrong files and

---
+++ sneaker_vue/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import client from '../views/client.vue'
+import login from '../views/login.vue'
+import register from '../views/register.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+      },
+    {
+      path: '/',
+      name: 'client',
+      component: client
+    
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component : register
+    },
+
+  ]
+})
+
+
+export default router
